3dd789adc30a3cd255197ba7a55b08babab374e8
h-mayorquin <h.mayorquin@gmail.com>
2018-03-15 14:13:23 +0000

    Noise diagram for understanding the robusiness of the system

--- plot_producers/simple_bcpnn_noise.py
+++ plot_producers/simple_bcpnn_noise.py
@@ -22,11 +22,11 @@ sns.set_style(style='white')
 markersize = 32
 linewidth = 10
 
-num = 25
-trials = 50
+num = 15
+trials = 25
 
 recall_noise1 = True
-recall_noise2 = True
+recall_noise2 = False
 minicolumns_noise = False
 ######
 ## General parameters
@@ -39,7 +39,7 @@ strict_maximum = True
 z_transfer = False
 
 # First we run the training protocol
-g_w_ampa = 2.0
+g_w_ampa = 1.5
 g_w = 0.0
 g_a = 10.0
 tau_a = 0.250
@@ -56,70 +56,84 @@ n_patterns = 10
 
 # Manager properties
 dt = 0.001
-values_to_save = ['o', 's']
-
-# Protocol
-training_time = 0.100
-inter_sequence_interval = 1.0
-inter_pulse_interval = 0.0
-resting_time = 0.0
-epochs = 3
+values_to_save = ['o']
 
 if recall_noise1:
     # Network parameters
+    w_diff_set = [1, 2, 3]
+    successes_list = []
+    persistent_time_list = []
     w_self = 1.0
-    w_next = -1.0
-    w_rest = -6
-    w_diff = g_w_ampa * (w_self  - w_next)
 
-    w = simple_bcpnn_matrix(minicolumns, w_self, w_next, w_rest)
+    for w_diff in w_diff_set:
+        w_next = w_self - w_diff
+        w_rest = -15.0
+        # w_diff = g_w_ampa * (w_self - w_next)
 
-    sigma_vector = np.linspace(0, w_diff, num=num)
-    success_vector =  np.zeros((num, trials))
-    persistent_time_vector = np.zeros((num, trials))
+        w = simple_bcpnn_matrix(minicolumns, w_self, w_next, w_rest)
 
-    for index_sigma, sigma in enumerate(sigma_vector):
-        for trial in range(trials):
-            # Build the network
-            nn = BCPNNPerfect(hypercolumns, minicolumns, g_w_ampa=g_w_ampa, g_w=g_w, g_a=g_a, tau_a=tau_a, sigma=sigma, G=G,
-                              tau_z_pre_ampa=tau_z_pre_ampa, tau_z_post_ampa=tau_z_post_ampa, tau_p=tau_p,
-                              z_transfer=z_transfer, diagonal_zero=False, strict_maximum=strict_maximum, perfect=perfect,
-                              k_perfect=k_perfect, always_learning=always_learning)
+        sigma_vector = np.linspace(0, (g_w_ampa * w_diff)/2.0, num=num)
+        success_vector = np.zeros((num, trials))
+        persistent_time_vector = np.zeros((num, trials))
 
-            # Build the manager
-            manager = NetworkManager(nn=nn, dt=dt, values_to_save=values_to_save)
-            nn.w_ampa = w
+        for index_sigma, sigma in enumerate(sigma_vector):
+            for trial in range(trials):
+                # Build the network
+                nn = BCPNNPerfect(hypercolumns, minicolumns, g_w_ampa=g_w_ampa, g_w=g_w, g_a=g_a, tau_a=tau_a, sigma=sigma, G=G,
+                                  tau_z_pre_ampa=tau_z_pre_ampa, tau_z_post_ampa=tau_z_post_ampa, tau_p=tau_p,
+                                  z_transfer=z_transfer, diagonal_zero=False, strict_maximum=strict_maximum, perfect=perfect,
+                                  k_perfect=k_perfect, always_learning=always_learning)
 
-            # Recall
-            T_recall = 2.2
-            T_cue = 0.080
-            sequences = [[i for i in range(n_patterns)]]
-            n = 1
+                # Build the manager
+                manager = NetworkManager(nn=nn, dt=dt, values_to_save=values_to_save)
+                nn.w_ampa = w
 
-            aux = calculate_recall_time_quantities(manager, T_recall, T_cue, n, sequences)
-            total_sequence_time, mean, std, success, timings = aux
+                # Recall
+                T_recall = 3.0
+                T_cue = 0.100
+                sequences = [[i for i in range(n_patterns)]]
+                n = 1
 
-            success_vector[index_sigma, trial] = success
-            persistent_time_vector[index_sigma, trial] = mean
+                aux = calculate_recall_time_quantities(manager, T_recall, T_cue, n, sequences)
+                total_sequence_time, mean, std, success, timings = aux
 
-    successes = success_vector.mean(axis=1) / 100.0
-    sigmas_norm = sigma_vector / w_diff
-    std = success_vector.std(axis=1) / 100.0
-    persistence =  persistent_time_vector.mean(axis=1)
+                success_vector[index_sigma, trial] = success
+                persistent_time_vector[index_sigma, trial] = mean
 
+        successes_list.append(success_vector)
+        persistent_time_list.append(persistent_time_vector)
+
+    # Plot
     fig1 = plt.figure(figsize=(16, 12))
     ax = fig1.add_subplot(111)
-    ax.plot(sigmas_norm, successes, 'o-', markersize=markersize, linewidth=linewidth, label='success')
-    ax.plot(sigmas_norm, persistence, 'o-', markersize=markersize, linewidth=linewidth, label=r'$T_{persistence}$')
+    ax2 = ax.twinx()
 
-    ax.fill_between(sigmas_norm, successes - std, successes + std, color='blue', alpha=0.1)
+    cmap = matplotlib.cm.Paired
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=len(w_diff_set))
 
-    ax.set_ylabel('Success')
-    ax.set_xlabel(r'$\frac{\sigma}{g_w w_{diff}}$')
+    for index, (w_diff, success_vector, persistent_time_vector) in enumerate(zip(w_diff_set, successes_list, persistent_time_list)):
+
+        successes = success_vector.mean(axis=1) / 100.0
+        sigma_vector = np.linspace(0, (g_w_ampa * w_diff)/2.0, num=num)
+        sigmas_norm = (2 * sigma_vector) / (g_w_ampa * w_diff)
+        std = success_vector.std(axis=1) / 100.0
+        persistence = persistent_time_vector.mean(axis=1)
+
+        ax.plot(sigmas_norm, successes, 'o-', color=cmap(norm(index)), markersize=markersize, linewidth=linewidth, label=r'$w_{diff} = $' + str(w_diff))
+        ax2.plot(sigmas_norm, persistence, '--', color=cmap(norm(index)), markersize=markersize, linewidth=linewidth)
+
+        ax.fill_between(sigmas_norm, successes - std, successes + std, color=cmap(norm(index)), alpha=0.25)
+
+    ax.set_ylabel('% Success / 100')
+    ax2.set_ylabel(r'$T_{persistence}$')
+    ax.set_xlabel(r'$\frac{2 \sigma}{g_w w_{diff}}$')
 
     ax.axhline(0, ls='--', color='gray')
     ax.axvline(0, ls='--', color='gray')
-    ax.axvline(1, ls='-', color='red', label=r'$g_w w_{diff}$')
+    ax.axvline(1, ls='-', color='red', label=r'$ \frac{g_w w_{diff}}{2}$')
+
+    ax.set_ylim([-0.1, 1.1])
+    ax2.set_ylim([-0.1, 1.1])
 
     ax.legend()
 
@@ -164,7 +178,7 @@ if recall_noise2:
 
     successes = success_vector.mean(axis=1) / 100.0
     sigmas_norm = sigma_vector / w_diff
-    std = success_vector.std(axis=1) / 100.0
+    std = success_vector.std(axis=1)
     persistence =  persistent_time_vector.mean(axis=1)
 
     fig2 = plt.figure(figsize=(16, 12))
---
+++ plot_producers/simple_bcpnn_noise_ilustration.py
@@ -0,0 +1,118 @@
+import sys
+sys.path.append('../')
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import seaborn as sns
+
+from network import Protocol, NetworkManager, BCPNNPerfect
+from analysis_functions import calculate_recall_time_quantities
+from connectivity_functions import simple_bcpnn_matrix
+
+sns.set(font_scale=2.8)
+sns.set_style(style='white')
+
+# Network structure
+always_learning = True
+k_perfect = True
+perfect = True
+strict_maximum = True
+z_transfer = False
+
+# First we run the training protocol
+g_w_ampa = 1.0
+g_w = 0.0
+g_a = 3.0
+g_I = 20.0
+tau_a = 0.150
+G = 1.0
+sigma = 0.0
+tau_z_pre_ampa = 0.005
+tau_z_post_ampa = 0.005
+tau_p = 10.0
+
+# Patterns parameters
+hypercolumns = 1
+minicolumns = 10
+n_patterns = 10
+
+# Manager properties
+dt = 0.001
+values_to_save = ['o', 's', 'i_ampa', 'i_nmda', 'beta', 'a']
+
+# Protocol
+training_time = 0.100
+inter_sequence_interval = 1.0
+inter_pulse_interval = 0.0
+resting_time = 0.0
+epochs = 3
+
+# Network parameters
+w_self = 1.0
+w_next = -1
+w_rest = -3.0
+w_diff = g_w_ampa * (w_self - w_next)
+
+# Build the network
+nn = BCPNNPerfect(hypercolumns, minicolumns, g_w_ampa=g_w_ampa, g_w=g_w, g_a=g_a, tau_a=tau_a, sigma=sigma, G=G,
+                  tau_z_pre_ampa=tau_z_pre_ampa, tau_z_post_ampa=tau_z_post_ampa, tau_p=tau_p, g_I=g_I,
+                  z_transfer=z_transfer, diagonal_zero=False, strict_maximum=strict_maximum, perfect=perfect,
+                  k_perfect=k_perfect, always_learning=always_learning)
+nn.g_beta = 0.0
+
+# Build the manager
+manager = NetworkManager(nn=nn, dt=dt, values_to_save=values_to_save)
+w = simple_bcpnn_matrix(minicolumns, w_self, w_next, w_rest)
+nn.w_ampa = w
+
+# Recall
+T_recall = 0.450
+T_cue = 0.050
+sequences = [[i for i in range(n_patterns)]]
+n = 1
+
+aux = calculate_recall_time_quantities(manager, T_recall, T_cue, n, sequences)
+total_sequence_time, mean, std, success, timings = aux
+
+i_ampa = manager.history['i_ampa']
+a = manager.history['a']
+time = np.linspace(0, manager.T_total, a.shape[0])
+
+##########
+# Plot
+##########
+patterns = [0, 1, 2]
+# patterns = sequences[0]
+
+w_diff = w_self - w_next
+sigma = 0.25 * g_w_ampa*  w_diff
+linewidth = 10
+current_palette = sns.color_palette()
+
+fig = plt.figure(figsize=(16, 12))
+
+ax = fig.add_subplot(111)
+
+for index, pattern in enumerate(patterns):
+    current = i_ampa[:, pattern] - g_a * a[:, pattern]
+    ax.plot(time, current, color=current_palette[index], linewidth=linewidth, label='current ' + str(index))
+    ax.fill_between(time, current - sigma, current + sigma, color=current_palette[index], alpha=0.25)
+
+
+ax.axhline(w_self, ls='--', linewidth=3,label=r'$g_w w_{self}$', color='black')
+ax.axhline(w_next, ls='-.', linewidth=3, label=r'$g_w w_{next}$', color='black')
+ax.axhline(w_rest, ls=':', linewidth=3, label=r'$g_w w_{rest}$', color='black')
+ax.set_title('Currents evolving in time')
+
+ax.legend(loc=3)
+ax.set_ylim([-8, 2])
+
+if True:
+    ax.axis('off')
+
+fig.savefig('./plot_producers/noise_diagram.pdf', frameon=False, dpi=110, bbox_inches='tight')
+
+
